d17dff48c8d0d68d0daf88601b700f51d74b48cf
Koen Vervoordeldonk <koenvervoordeldonk93@gmail.com>
2016-04-13 21:59:50 +0200

    Added a PUT request to close a request

--- src/BsTask/src/main/java/com/projectb/repositories/RequestRepo.java
+++ src/BsTask/src/main/java/com/projectb/repositories/RequestRepo.java
@@ -2,9 +2,10 @@ package com.projectb.repositories;
 
 import com.projectb.entities.Request;
 import com.projectb.repositories.abs.TaskRepo;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface RequestRepo extends TaskRepo<Request> {
-
+    //TODO: If you want specific things of the RequestRepo
 }
--- src/PcsTask/src/main/java/com/projectb/endpoint/RequestService.java
+++ src/PcsTask/src/main/java/com/projectb/endpoint/RequestService.java
@@ -5,11 +5,11 @@ import com.projectb.entities.Request;
 import com.projectb.repo.BasicRepo;
 import com.projectb.repositories.RequestRepo;
 import com.projectb.vault.AbsRestVault;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "requests", produces = "application/json")
@@ -23,6 +23,24 @@ public class RequestService extends TaskService<Request> {
 //    public BasicRepo<Request> provideRepo() {
 //        return requestRepo;
 //    }
+
+    @Autowired
+    private RequestRepo requestRepo;
+
+    @RequestMapping(value= "{id}/close", method = RequestMethod.PUT)
+    public ResponseEntity<String> closeRequest(@PathVariable Long id) {
+        Request request = requestRepo.findOne(id);
+        if(request != null) {
+            close(request);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    private void close(Request request) {
+        request.setClosed(true);
+        requestRepo.save(request);
+    }
 
     @RequestMapping(method = RequestMethod.POST, consumes = "application/json")
     public Request insert(@RequestBody Request request) {
