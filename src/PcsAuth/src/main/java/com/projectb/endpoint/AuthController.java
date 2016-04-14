a751874d050230207e59f971efbb31379365fd39
Mike Rombout <mike.rombout1@gmail.com>
2016-04-14 11:46:07 +0200

    Add basic form auth

---
+++ src/PcsAuth/src/main/java/com/projectb/endpoint/AuthController.java
@@ -0,0 +1,13 @@
+package com.projectb.endpoint;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.security.Principal;
+
+public class AuthController {
+
+}
