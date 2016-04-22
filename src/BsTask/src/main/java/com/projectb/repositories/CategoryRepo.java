827974ae58cde75e61dd546ac4cc2c81fd364c01
Mike Rombout <mike.rombout1@gmail.com>
2016-04-22 14:50:48 +0200

    Add categories READ

--- src/BsTask/src/main/java/com/projectb/repositories/CategoryRepo.java
+++ src/BsTask/src/main/java/com/projectb/repositories/CategoryRepo.java
@@ -2,9 +2,10 @@ package com.projectb.repositories;
 
 import com.projectb.entities.Category;
 import com.projectb.entities.Offer;
+import com.projectb.repo.BasicRepo;
 import com.projectb.repo.CRUDRepo;
 import com.projectb.repositories.abs.TaskRepo;
 
-public interface CategoryRepo extends CRUDRepo<Category> {
+public interface CategoryRepo extends BasicRepo<Category> {
 
 }
