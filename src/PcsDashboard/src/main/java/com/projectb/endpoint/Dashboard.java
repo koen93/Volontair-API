2d101a70df3ef94e9b9f1aaf42836c57e78ee09f
Mike Rombout <mike.rombout1@gmail.com>
2016-05-03 20:32:47 +0200

    Replace faulty import

--- src/PcsDashboard/src/main/java/com/projectb/endpoint/Dashboard.java
+++ src/PcsDashboard/src/main/java/com/projectb/endpoint/Dashboard.java
@@ -1,9 +1,9 @@
 package com.projectb.endpoint;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.projectb.entities.Conversation;
 import com.projectb.entities.Message;
 import lombok.Getter;
-import sun.plugin2.message.Conversation;
 
 import java.util.ArrayList;
 import java.util.List;
